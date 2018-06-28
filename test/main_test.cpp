cb978a0fca5ceca020bbf06eecac088debb6f1e3
Cliff Green <softwarelibre@codewrangler.net>
2018-06-28 12:48:57 -0700

    Initial creation and commit.

---
+++ test/main_test.cpp
@@ -0,0 +1,21 @@
+/** @file
+ *
+ *  @defgroup test_module Unit tests for the various Chops components.
+ *
+ *  @ingroup test_module
+ *
+ *  @brief Source file for combining (i.e. linking) all Chops tests together.
+ *
+ *  @author Cliff Green
+ *
+ *  Copyright (c) 2017-2018 by Cliff Green
+ *
+ *  Distributed under the Boost Software License, Version 1.0. 
+ *  (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+

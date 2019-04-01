0c39e140e7b22b38c9ab5055ccf7862cf01e2fbe
Cliff Green <softwarelibre@codewrangler.net>
2019-03-31 22:14:00 -0700

    Added include guard, chops namespace, and clarified doxygen.

--- include/utility/cast_ptr_to.hpp
+++ include/utility/cast_ptr_to.hpp
@@ -1,14 +1,20 @@
 /** @file
  * 
- *  @defgroup utility_module
+ *  @defgroup utility_module Simple utility classes and functions.
+ *
+ *  @ingroup utility_module
  * 
- *  @brief Utility functions for safely casting a pointer.
+ *  @brief Utility functions for safely casting a pointer between types, primarily 
+ *  from some type T to either a @c char* or @c std::byte*.
  * 
  *  C++ @c reinterpret_cast can cast any pointer to another pointer type, but
  *  the behaviour can be undefined. C++ guarentees that a pointer can be safely
- *  cast to either @c void* or @c char*. That pointer can be cast to another
- *  type, or cast back to the original pointer type.
- * 
+ *  cast to either @c void* or @c char* (or @c std::byte*, which is a form of @c char*), 
+ *  and that pointer can be cast back to the original pointer type. Casting between
+ *  two unrelated types (outside of the @c void* and @c char* conversions already 
+ *  mentioned) results in undefined behavior (even if functionally the memory layout is 
+ *  the same and execution is successful on a given platform).
+ *  
  *  Both const and non-const overloads are available.
  * 
  *  @authors Thurman Gillespy, Cliff Green
@@ -20,6 +26,10 @@
  * 
  */
 
+#ifndef CAST_PTR_TO_HPP_INCLUDED
+#define CAST_PTR_TO_HPP_INCLUDED
+
+namespace chops {
 
 template <typename Dst, typename Src>
 const Dst* cast_ptr_to (const Src* p) {
@@ -30,3 +40,8 @@ template <typename Dst, typename Src>
 Dst* cast_ptr_to (Src* p) {
     return static_cast<Dst*> (static_cast<void*> (p));
 }
+
+} // end namespace
+
+#endif
+

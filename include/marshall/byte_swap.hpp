838e0b8d36e400f08ee69746ef8887bb1f586815
Cliff Green <softwarelibre@codewrangler.net>
2019-04-04 20:04:08 -0700

    Working on extract val functions.

--- include/marshall/byte_swap.hpp
+++ include/marshall/byte_swap.hpp
@@ -5,7 +5,9 @@
  *
  *  @ingroup marshall_module
  *
- *  @brief Byte swapping and endian detection utility functions.
+ *  @brief Byte Extract fundamental integral values from a byte buffer, and the converse
+ *  process of taking a fundamental value and pushing it on to a buffer of bytes in network
+ *  endian order.
  *
  *  @author Cliff Green
  *
@@ -30,66 +32,120 @@ namespace chops {
 
 namespace detail {
 
-// since function template partial specialization is not supported (yet) in C++,
-// overload the swap function template with a parameter corresponding to the sizeof the
-// value being swapped; no data is passed in the second parameter, only using the type 
-// for overloading
+// since function template partial specialization is not supported (yet) in C++, overload the 
+// extract_val_swap and noswap function templates with a parameter corresponding to the 
+// sizeof the value; no data is passed in the second parameter, only using the type for overloading
 
 template <unsigned int>
 struct size_tag { };
 
 // if char / byte, no swap needed
 template <typename T>
-constexpr void byte_swap(T&, const size_tag<1u>*) noexcept {
+constexpr T extract_val_swap(const std::byte* buf, const size_tag<1u>*) noexcept {
+  return static_cast<T>{*buf}; // some char type
 }
-
-// std::swap is not constexpr until C++ 20, so the following functions won't be
-// constexpr until then; however, it doesn't hurt to think ahead
 template <typename T>
-constexpr void byte_swap(T& val, const size_tag<2u>*) noexcept {
-  std::byte* p = cast_ptr_to<std::byte>(&val);
-  std::swap (*(p+0), *(p+1));
+constexpr T extract_val_noswap(const std::byte* buf, const size_tag<1u>*) noexcept {
+  return static_cast<T>{*buf}; // some char type
 }
 
 template <typename T>
-constexpr void byte_swap(T& val, const size_tag<4u>*) noexcept {
-  std::byte* p = cast_ptr_to<std::byte>(&val);
-  std::swap (*(p+0), *(p+3));
-  std::swap (*(p+1), *(p+2));
+constexpr T extract_val_swap(const std::byte* buf, const size_tag<2u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+1);
+  *(p+1) = *(buf+0);
+  return tmp;
 }
 
 template <typename T>
-constexpr void byte_swap(T& val, const size_tag<8u>*) noexcept {
-  std::byte* p = cast_ptr_to<std::byte>(&val);
-  std::swap (*(p+0), *(p+7));
-  std::swap (*(p+1), *(p+6));
-  std::swap (*(p+2), *(p+5));
-  std::swap (*(p+3), *(p+4));
+constexpr T extract_val_noswap(const std::byte* buf, const size_tag<2u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+0);
+  *(p+1) = *(buf+1);
+  return tmp;
 }
 
 template <typename T>
-constexpr void byte_swap(T& val, const size_tag<16u>*) noexcept {
-  std::byte* p = cast_ptr_to<std::byte>(&val);
-  std::swap (*(p+0), *(p+15));
-  std::swap (*(p+1), *(p+14));
-  std::swap (*(p+2), *(p+13));
-  std::swap (*(p+3), *(p+12));
-  std::swap (*(p+4), *(p+11));
-  std::swap (*(p+5), *(p+10));
-  std::swap (*(p+6), *(p+9));
-  std::swap (*(p+7), *(p+8));
+constexpr T extract_val_swap(const std::byte* buf, const size_tag<4u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+3);
+  *(p+1) = *(buf+2);
+  *(p+2) = *(buf+1);
+  *(p+3) = *(buf+0);
+  return tmp;
+}
+
+template <typename T>
+constexpr T extract_val_noswap(const std::byte* buf, const size_tag<4u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+0);
+  *(p+1) = *(buf+1);
+  *(p+2) = *(buf+2);
+  *(p+3) = *(buf+3);
+  return tmp;
+}
+
+template <typename T>
+constexpr T extract_val_swap(const std::byte* buf, const size_tag<8u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+7);
+  *(p+1) = *(buf+6);
+  *(p+2) = *(buf+5);
+  *(p+3) = *(buf+4);
+  *(p+4) = *(buf+3);
+  *(p+5) = *(buf+2);
+  *(p+6) = *(buf+1);
+  *(p+7) = *(buf+0);
+  return tmp;
+}
+
+template <typename T>
+constexpr T extract_val_noswap(const std::byte* buf, const size_tag<8u>*) noexcept {
+  T tmp{};
+  std::byte* p = cast_ptr_to<std::byte>(&tmp);
+  *(p+0) = *(buf+0);
+  *(p+1) = *(buf+1);
+  *(p+2) = *(buf+2);
+  *(p+3) = *(buf+3);
+  *(p+4) = *(buf+4);
+  *(p+5) = *(buf+5);
+  *(p+6) = *(buf+6);
+  *(p+7) = *(buf+7);
+  return tmp;
 }
 
 } // end namespace detail
 
+// C++ 20 will contain std::endian, which allows full compile time endian detection;
+// until then, the endian detection and branching will be runtime, although it
+// can be computed at global initialization instead of each time it is called
+
+inline bool detect_big_endian () noexcept {
+  const std::uint32_t tmp = 0xDDCCBBAA;
+  return *(cast_ptr_to<std::byte>(&tmp) + 3) == static_cast<std::byte>(0xDD);
+}
+
+// should be computed at global initialization time
+
+const bool big_endian = detect_big_endian();
+
 /**
- * @brief Unconditional in-place byte swapping of integral types.
- *
- * The @c byte_swap functions do not check for endianness, so conditional swapping based on
- * endianness should be performed at a higher level.
+ * @brief Extract a value in network byte order (big endian) from a @c std::byte buffer 
+ * into a fundamental integral type in native endianness.
  *
  * This function template dispatches on specific sizes. If an unsupported size is attempted
  * to be swapped, a static error is generated.
+ *
+ * @param buf Buffer of @c std::bytes containing an object of type T in network byte order.
+ *
+ * @return A value in native endian order.
+ *
+ * @pre The buffer must contain at least @c sizeof(T) bytes.
  *
  * @note Floating point swapping is not supported, only integral types. Swapping floating
  * point types can easily result in NaN representations, which can generate hardware traps,
@@ -101,31 +157,15 @@ constexpr void byte_swap(T& val, const size_tag<16u>*) noexcept {
  *
  */
 template <typename T>
-constexpr void byte_swap(T& val) noexcept {
+constexpr T extract_val(const std::byte* buf) noexcept {
 
-  static_assert(sizeof(val) == 1u || sizeof(val) == 2u || sizeof(val) == 4u || sizeof(val) == 8u || sizeof(val) == 16u,
+  static_assert(sizeof(val) == 1u || sizeof(val) == 2u || sizeof(val) == 4u || sizeof(val) == 8u,
     "Size for swapping is not supported.");
   static_assert(std::is_integral<T>::value, "Swapping is only supported for integral types.");
 
-  detail::byte_swap(val, const detail::size_tag<sizeof(val)>*(nullptr));
+  return big_endian ? detail::extract_val_noswap(buf, const detail::size_tag<sizeof(T)>*(nullptr)),
+                      detail::extract_val_swap(buf, const detail::size_tag<sizeof(T)>*(nullptr));
 }
-
-
-// C++ 20 will contain std::endian, which allows full compile time endian detection; in
-// addition, C++ 20 std::swap will be constexpr which allow additional compile time
-// optimizations; until then, the endian detection and branching will be runtime
-
-inline bool detect_big_endian () {
-  const std::uint32_t orig = 0xDDCCBBAA;
-  std::uint32_t tmp { orig };
-  byte_swap(tmp);
-  return tmp == orig;
-}
-
-// while the following is not computed at runtime, it will be computed at global
-// initialization time
-
-const bool big_endian = detect_big_endian();
 
 } // end namespace
 

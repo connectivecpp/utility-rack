64b01a8e3464fbaf99596e2b54e36b1c415f98ea
Roxanne Agerone <argonkryptonneon@gmail.com>
2019-06-10 18:13:32 -0700

    adding decode and encode variable length int

--- include/marshall/extract_append.hpp
+++ include/marshall/extract_append.hpp
@@ -201,6 +201,7 @@ std::size_t append_val_noswap(std::byte* buf, const T& val, const size_tag<8u>*)
   return 8u;
 }
 
+
 } // end namespace detail
 
 // C++ 20 will contain std::endian, which allows full compile time endian detection;
@@ -276,6 +277,76 @@ std::size_t append_val(std::byte* buf, const T& val) noexcept {
                       detail::append_val_swap(buf, val, static_cast< const detail::size_tag<sizeof(T)>* >(nullptr));
 }
 
+// template <typename T>
+// T extract_variable_len_val(const std::byte* buf) noexcept{
+// // T variable_temp
+// // return variable_temp
+// }
+
+
+// template <typename T>
+// std::size_t append_variable_len_val(const T& val) noexcept{
+//   std::size_t count = 0 
+
+//     while (val > 127) {
+//       //|128: Set the next byte flag
+//       output[count] = ((T)(val & 127)) | 128;
+//       //Remove the seven bits we just wrote
+//       val >>= 7;
+//       count++;
+//     }
+//     count++;
+//     return count;
+// // return std_size_t
+// }
+
+
+/**
+ * * License: CC0 1.0 Universal
+ * Originally published on http://techoverflow.net
+ * Copyright (c) 2015 Uli Koehler
+ * Encodes an unsigned variable-length integer using the MSB algorithm.
+ * This function assumes that the value is stored as little endian.
+ * @param value The input value. Any standard integer type is allowed.
+ * @param output A pointer to a piece of reserved memory. Must have a minimum size dependent on the input size (32 bit = 5 bytes, 64 bit = 10 bytes).
+ * @return The number of bytes used in the output memory.
+ * 
+ */
+template<typename T>
+std::size_t encode_var_int( std::byte* output, T val) {
+    std::size_t outputSize = 0;
+    //While more than 7 bits of data are left, occupy the last output byte
+    // and set the next byte flag
+    while (val > 127) {
+        //|128: Set the next byte flag
+        output[outputSize] = ((uint8_t)(value & 127)) | 128;
+        //Remove the seven bits we just wrote
+        val >>= 7;
+        outputSize++;
+    }
+    output[outputSize++] = ((uint8_t)value) & 127;
+    return outputSize;
+}
+/**
+ * * License: CC0 1.0 Universal
+ * Originally published on http://techoverflow.net
+ * Copyright (c) 2015 Uli Koehler
+ * Decodes an unsigned variable-length integer using the MSB algorithm.
+ * @param value A variable-length encoded integer of arbitrary size.
+ * @param inputSize How many bytes are 
+ */
+template<typename T>
+T decode_var_int(const std::byte* input, std::size_t inputSize) {
+    T ret = 0;
+    for (std::size_t i = 0; i < inputSize; i++) {
+        ret |= (input[i] & 127) << (7 * i);
+        //If the next-byte flag is set
+        if(!(input[i] & 128)) {
+            break;
+        }
+    }
+    return ret;
+}
 } // end namespace
 
 #endif

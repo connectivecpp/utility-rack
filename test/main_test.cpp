70563fbdc6fc864b4ec8bcb073697748b5f77103
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2019-07-25 08:27:03 -0700

    Tweaked doxygen comments on defgroup command.

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,6 +1,6 @@
 /** @file
  *
- *  @defgroup test_module Unit tests for the various Chops components.
+ *  @defgroup test_module Unit tests for the various Chops Utility Rack components.
  *
  *  @ingroup test_module
  *
@@ -8,7 +8,7 @@
  *
  *  @author Cliff Green
  *
- *  Copyright (c) 2017-2018 by Cliff Green
+ *  Copyright (c) 2017-2019 by Cliff Green
  *
  *  Distributed under the Boost Software License, Version 1.0. 
  *  (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)

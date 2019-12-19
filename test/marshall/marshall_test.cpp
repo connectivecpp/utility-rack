b5a0ac7a11aae41ce8f800f416e8769b7567c84c
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2019-12-19 11:03:33 -0800

    Working on marshalling.

--- test/marshall/marshall_test.cpp
+++ test/marshall/marshall_test.cpp
@@ -32,20 +32,15 @@ struct loc {
   short  altitude;
 };
 
-namespace chops {
+namespace hiking {
 
 template <typename Buf>
 Buf& marshall(Buf& buf, const loc& loc) {
-  marshall<std::int32_t>(buf, loc.latitude);
-  marshall<std::int32_t>(buf, loc.longitude);
-  marshall<std::int16_t>(buf, loc.altitude);
+  chops::marshall<std::int32_t>(buf, loc.latitude);
+  chops::marshall<std::int32_t>(buf, loc.longitude);
+  chops::marshall<std::int16_t>(buf, loc.altitude);
   return buf;
 }
-
-} // end namespace chops
-
-
-namespace hiking {
 
 struct trail_stats {
   long                length;
@@ -61,29 +56,25 @@ struct hiking_trail {
   trail_stats     stats;
 };
 
-} // end namespace hiking
-
-namespace chops {
-
 template <typename Buf>
-Buf& marshall(Buf& buf, const hiking::trail_stats& ts) {
-  marshall<std::uint64_t>(buf, ts.length);
-  marshall<std::uint16_t>(buf, ts.elev);
-  marshall<std::uint8_t, std::uint16_t>(buf, ts.rating);
+Buf& marshall(Buf& buf, const trail_stats& ts) {
+  chops::marshall<std::uint64_t>(buf, ts.length);
+  chops::marshall<std::uint16_t>(buf, ts.elev);
+  chops::marshall<std::uint8_t, std::uint16_t>(buf, ts.rating);
   return buf;
 }
 
 template <typename Buf>
-Buf& marshall(Buf& buf, const hiking::hiking_trail& ht) {
-  marshall<std::uint16_t>(buf, ht.name);
-  marshall<std::uint8_t>(buf, ht.federal);
+Buf& marshall(Buf& buf, const hiking_trail& ht) {
+  chops::marshall<std::uint16_t>(buf, ht.name);
+  chops::marshall<std::uint8_t>(buf, ht.federal);
   marshall(buf, ht.trail_head);
-  marshall_seq<std::uint16_t, loc>(buf, ht.intersections.size(), ht.intersections.cbegin());
+  chops::marshall_seq<std::uint16_t, loc>(buf, ht.intersections.size(), ht.intersections.cbegin());
   marshall(buf, ht.stats);
   return buf;
 }
 
-} // end namespace chops
+} // end namespace hiking
 
 template <typename Buf>
 void test_marshall () {
@@ -99,14 +90,14 @@ void test_marshall () {
   const loc pt1 { 42, 43, 21 };
   const loc pt2 { 62, 63, 11 };
 
-  chops::marshall(buf, pt1);
-  chops::marshall(buf, pt2);
+  hiking::marshall(buf, pt1);
+  hiking::marshall(buf, pt2);
 
   const hiking::trail_stats ts1 { 101, 51, std::make_optional(201) };
   const hiking::trail_stats ts2 { 301, 41, std::make_optional(401) };
 
-  chops::marshall(buf, ts1);
-  chops::marshall(buf, ts2);
+  hiking::marshall(buf, ts1);
+  hiking::marshall(buf, ts2);
 
   const loc inter1 { 1001, 1002, 500 };
   const loc inter2 { 1003, 1004, 501 };
@@ -119,8 +110,8 @@ void test_marshall () {
   const hiking::hiking_trail hk1 { "Huge trail", true, pt1, { inter1, inter2, inter3 }, ts1 };
   const hiking::hiking_trail hk2 { "Small trail", false, pt2, { inter3, inter4, inter5, inter6 }, ts2 };
 
-  chops::marshall(buf, hk1);
-  chops::marshall(buf, hk2);
+  hiking::marshall(buf, hk1);
+  hiking::marshall(buf, hk2);
 
 }
 

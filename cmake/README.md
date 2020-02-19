cff243325935aadbbcc586c2b562bbca1526a681
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2020-02-19 10:44:05 -0800

    Refactoring CMake files.

--- cmake/README.md
+++ cmake/README.md
@@ -1,4 +1,5 @@
 ## CMake Utilities
 
-Shared CMake utility code is in this directory. This will expand as our knowledge and skill with CMake increases.
+Shared CMake utility code is in this directory. The `all_repos` directory in Utility Rack repo contains CMake code that is shared for all Connective C++ repos.
 
+The contents of this directory will expand as our knowledge and skill with CMake increases.
--- cmake/unit_test_header_dirs_var.cmake
+++ cmake/unit_test_header_dirs_var.cmake
@@ -5,11 +5,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-set ( test_main_src "${test_source_dir}/main_test.cpp" )
-set ( tester_suffix "_runner" )
-set ( main_test_lib_name "main_test_lib" )
-
-set ( header_dirs
+set ( unit_test_header_dirs
     "${include_source_dir}"
     "${third_party_include_source_dir}"
     )

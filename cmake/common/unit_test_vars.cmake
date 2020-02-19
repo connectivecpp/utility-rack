bd5af13fa3886cd5a739f64d5ddbd8dde1702846
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2020-02-18 16:52:11 -0800

    Working on refactoring CMake files.

---
+++ cmake/common/unit_test_vars.cmake
@@ -0,0 +1,18 @@
+# Copyright 2020 by Cliff Green
+#
+# https://github.com/connectivecpp/utility-rack
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+set ( test_main_src "${test_source_dir}/main_test.cpp" )
+set ( tester_suffix "_runner" )
+set ( main_test_lib_name "main_test_lib" )
+
+set ( header_dirs
+    "${include_source_dir}"
+    "${third_party_include_source_dir}"
+    )
+
+# end of file
+

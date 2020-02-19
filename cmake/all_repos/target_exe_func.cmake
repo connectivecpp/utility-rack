635579c496be630547363bbe23067cd7ea446bc1
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2020-02-19 14:03:14 -0800

    Refactored CMake files, some function renames.

--- cmake/all_repos/target_exe_func.cmake
+++ cmake/all_repos/target_exe_func.cmake
@@ -5,21 +5,17 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# Note - pthread library link will soon be cleaned up
-
-function ( make_exe target src )
+function ( target_exe target src )
     add_executable        ( ${target} ${src} )
     add_target_info       ( ${target} )
-    target_link_libraries ( ${target} PRIVATE pthread )
-    message ( "Executable to create: ${target}" )
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+    find_package(Threads REQUIRED)
+    target_link_libraries ( ${target} PRIVATE Threads::Threads)
 endfunction()
 
-function ( make_unit_test_exe target src )
-    add_executable        ( ${target} ${src} )
-    add_target_info       ( ${target} )
-    target_link_libraries ( ${target} PRIVATE pthread )
+function ( unit_test_target_exe target src )
+    target_exe ( ${target} ${src} )
     target_link_libraries ( ${target} PRIVATE ${main_test_lib_name} )
-    message ( "Unit test executable to create: ${target}" )
     add_test ( NAME ${target}${tester_suffix} COMMAND ${target} )
 endfunction()
 

0661beb34659eb100e31fd0699eaa2624ac41752
cliffg-softwarelibre <cliffg-softwarelibre@users.noreply.github.com>
2025-04-04 23:54:47 +0000

    deploy: 0f8d4c48c87488f4d5f47181b82d0256df489f31

--- search/all_6.js
+++ search/all_6.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['overloaded_0',['overloaded',['../structchops_1_1overloaded.html',1,'chops::overloaded&lt; Ts &gt;'],['../index.html#autotoc_md5',1,'Overloaded']]],
-  ['overloaded_2ehpp_1',['overloaded.hpp',['../overloaded_8hpp.html',1,'']]],
-  ['overloaded_5ftest_2ecpp_2',['overloaded_test.cpp',['../overloaded__test_8cpp.html',1,'']]],
-  ['overview_3',['Utility Rack Overview',['../index.html',1,'']]]
+  ['overloaded_0',['Overloaded',['../index.html#autotoc_md5',1,'']]],
+  ['overloaded_1',['overloaded',['../structchops_1_1overloaded.html',1,'chops']]],
+  ['overloaded_2ehpp_2',['overloaded.hpp',['../overloaded_8hpp.html',1,'']]],
+  ['overloaded_5ftest_2ecpp_3',['overloaded_test.cpp',['../overloaded__test_8cpp.html',1,'']]],
+  ['overview_4',['Utility Rack Overview',['../index.html',1,'']]]
 ];
